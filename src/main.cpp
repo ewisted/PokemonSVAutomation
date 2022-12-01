b427560ac83147f179bb22502cabad03377d92b3
ewisted <ew@atuw.org>
2022-11-30 23:58:11 -0600

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include <Arduino.h>
+#include <NintendoSwitchControlLibrary.h>
+
+void setup() {
+  // put your setup code here, to run once:
+  pushButton(Button::A, 3000);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  
+}
